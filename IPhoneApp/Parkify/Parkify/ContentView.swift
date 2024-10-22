128a656bfb35b1e810346164bdcf030b41f6ac95
Ayyan Malik <malikayyan@gmail.com>
2024-10-21 20:28:31 -0700

    adding Iphone app swiftUI and xcode files

---
+++ IPhoneApp/Parkify/Parkify/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Parkify
+//
+//  Created by Ayyan Malik on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("UNLV PARKIFY").font(
+            .largeTitle)
+        .foregroundColor(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IPhoneApp/Parkify/Parkify/ParkifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkifyApp.swift
+//  Parkify
+//
+//  Created by Ayyan Malik on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
