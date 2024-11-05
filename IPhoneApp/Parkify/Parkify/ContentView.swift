f6df0160968c8fe4d0c6b5a1c91b046e34423302
HumphreyTheKing <122954089+HumphreyTheKing@users.noreply.github.com>
2024-11-05 12:26:30 -0800

    switch to React

--- IPhoneApp/Parkify/Parkify/ContentView.swift
+++ IPhoneApp/Parkify/Parkify/ContentView.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
+
+struct ParkingGarage: Identifiable {
+    let id = UUID()
+    let title: String
+    let address: String
+    let status: String
+    let spots: Int
+    let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    @StateObject private var viewModel = ParkingViewMode()
+
     var body: some View {
-        Text("UNLV PARKIFY").font(
-            .largeTitle)
-        .foregroundColor(.red)
+        NavigationView {
+            VStack(spacing: 0){
+                HStack {
+                    Text("Parkify")
+                    .font(.title2)
+                    .forebackgroundColor(.white)
+                    .padding(.leading)
+                Spacer()
+            }
+            .frame(height: 50)
+            .background(Color.red.opacity(0.9))
+
+            
+            }
+        }
     }
 }
 
